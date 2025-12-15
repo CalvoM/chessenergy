5413d3475a70bbdee4c37dd0299ff13fabe549be
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-12-16 00:19:10 +0300

    test: let the actions be added.

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath } from "node:url";
 import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
 import viteConfig from "./vite.config";
+import path from "path";
 
 export default mergeConfig(
   viteConfig,
